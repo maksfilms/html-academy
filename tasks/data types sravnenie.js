ac95017eff848f861d5f416f26a9f416dcc5d9fa
Maksim Plysheuski <plysheuski.maksim@gmail.com>
2022-05-16 21:26:51 +0300

    solution of the task

---
+++ tasks/data types sravnenie.js
@@ -0,0 +1,8 @@
+console.log(true <= 0);
+console.log(false <= 0);
+console.log('2' > '1');
+console.log('авто' < 'АВТО');
+console.log('авто' < 'АВТОМОБИЛЬ');
+console.log('авто' < 'автомобиль');
+
+// understanding what is bigger
