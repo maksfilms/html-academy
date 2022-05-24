95acb8fcea75c0dbad6d339ab2ccb07fcaf49839
Maksim Plysheuski <plysheuski.maksim@gmail.com>
2022-05-24 23:29:22 +0300

    solution of the task

---
+++ tasks/flight time.js
@@ -0,0 +1,21 @@
+let flightDistance = 7260;
+let averageSpeed = 600;
+
+let flightTime = Math.round(flightDistance / averageSpeed);
+console.log(flightTime);
+
+
+
+/* Техническое задание
+
+Посчитай сколько времени займёт мой перелёт.
+
+В переменную flightDistance записано расстояние полёта в километрах.
+
+В переменной averageSpeed находится средняя скорость самолёта (километры в час).
+
+Найди время полёта (в часах) и запиши его в переменную flightTime.
+
+Округляй результат вычислений с помощью команды Math.round.
+
+*/
