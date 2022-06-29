6a74a2c0ffe82990bca30d096bda854957e56cb0
Maksim Plysheuski <plysheuski.maksim@gmail.com>
2022-06-29 21:38:34 +0300

    solution of the task

---
+++ tasks/if else condition.js
@@ -0,0 +1,23 @@
+let optimalTime = 80;
+let walkTime = 80;
+
+if (walkTime >= optimalTime) {
+    console.log("Прогулка достаточно длинная");
+} else {
+    console.log("Нужно ещё погулять");
+}
+
+
+/* v2.0
+let optimalTime = 80;
+let walkTime = 20
+let timeLeft;
+
+if (walkTime >= optimalTime) {
+  timeLeft = 0;
+} else {
+  timeLeft = optimalTime - walkTime;
+}
+
+console.log('Осталось гулять ещё ' + timeLeft + ' минут.');
+*/
