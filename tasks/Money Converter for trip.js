b641cee01f500558622b1535ab46bea94303792e
Maksim Plysheuski <plysheuski.maksim@gmail.com>
2022-06-29 21:39:22 +0300

    solution of the task

---
+++ tasks/Money Converter for trip.js
@@ -0,0 +1,25 @@
+let euroRate = 74;
+let dollarRate = 63;
+
+let euroAmount = 500;
+let dollarAmount = 2500;
+
+
+let rublesAmount = (dollarAmount * dollarRate) + (euroAmount * euroRate);
+
+console.log(rublesAmount);
+
+
+
+
+/* Техническое задание
+
+Напиши программу, которая посчитает сколько всего рублей понадобится на поездку.
+
+Курсы валют указаны в переменных euroRate и dollarRate.
+
+Переменные euroAmount и dollarAmount— необходимые суммы на поездку.
+
+Создай переменную rublesAmount и записывай в неё результат вычислений.
+
+*/
