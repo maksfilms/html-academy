516b148fb036121ba5687739e39bcbf11f872278
Maksim Plysheuski <plysheuski.maksim@gmail.com>
2022-07-02 21:30:23 +0300

    solution of the task

--- tasks/array sort 2.js
+++ tasks/array sort 2.js
@@ -1,10 +1,10 @@
 let numbers = [4, 6, 2, 11, 1, 3, 7, 12, 10, 9, 5, 8];
-console.log('Массив до сортировки: ' + numbers);
+console.log("Массив до сортировки: " + numbers);
 
-for (let i = 0; i <= numbers.length -1; i++) {
+for (let i = 0; i <= numbers.length - 1; i++) {
     let min = numbers[i];
 
-    for (let j = i + 1; j <= numbers.length -1; j++) {
+    for (let j = i + 1; j <= numbers.length - 1; j++) {
         if (numbers[j] < min) {
             min = numbers[j];
             let forSwap = numbers[i];
@@ -13,4 +13,4 @@ for (let i = 0; i <= numbers.length -1; i++) {
         }
     }
 }
-console.log('Массив после сортировки: ' + numbers);
+console.log("Массив после сортировки: " + numbers);
