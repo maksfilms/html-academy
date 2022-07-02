b2ae4fe336bff431efe163aa9f151e8d7ba35395
Maksim Plysheuski <plysheuski.maksim@gmail.com>
2022-07-02 21:31:23 +0300

    solution of the task

--- tasks/arrrays sort.js
+++ tasks/arrrays sort.js
@@ -1,18 +1,18 @@
-let numbers = [4, 2, 1, 3, 6, 7, 9 , 5 , 8];
+let numbers = [4, 2, 1, 3, 6, 7, 9, 5, 8];
 console.log(numbers);
 
 
 for (let index = 0; index <= numbers.length - 1; index++) {
     let minNumber = numbers[index];
 
-for (let j = index + 1; j <= numbers.length - 1; j++) {
-  if (numbers[j] < minNumber) {
-    minNumber = numbers[j];
-    let swap = numbers[index];
-    numbers[index] = minNumber;
-    numbers[j] = swap;
-  }
-}
+    for (let j = index + 1; j <= numbers.length - 1; j++) {
+        if (numbers[j] < minNumber) {
+            minNumber = numbers[j];
+            let swap = numbers[index];
+            numbers[index] = minNumber;
+            numbers[j] = swap;
+        }
+    }
 
 }
 
