e943b0188b121d281df126bfddf014719f6a7b76
Maksim Plysheuski <plysheuski.maksim@gmail.com>
2022-07-07 23:56:57 +0300

    solution of the task

---
+++ tasks/Objects 5.js
@@ -0,0 +1,12 @@
+let buildComputer = function(memory, display, processor) {
+  
+  return {
+          basicPrice: 5000,
+          processor: processor,
+          display: display,
+          memory: memory
+      };
+    }
+    
+  let myComputer = buildComputer(8, 13, 'i7');
+  console.log(myComputer)
